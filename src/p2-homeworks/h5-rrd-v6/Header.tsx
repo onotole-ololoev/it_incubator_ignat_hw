6c31983ef918cc22fa0d44106583d14766d1ab76
Anatoli Nikolaevich <tolya.tar@gmail.com>
2022-04-29 13:00:17 +0300

    transform header added

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,24 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {PATH} from "./Pages";
 import {Link} from "react-router-dom";
 import s from './../../p1-main/m1-ui/u1-app/App.module.css'
 
 function Header() {
-    return (
-        <div className={s.header}>
-            <div className={s.link}>
-                <Link to={PATH.PRE_JUNIOR}>pre-junior</Link>
-            </div>
-            <div className={s.link}>
-                <Link to={PATH.JUNIOR}>junior</Link>
-            </div>
-            <div className={s.link}>
-                <Link to={PATH.JUNIOR_PLUS}>junior+</Link>
-            </div>
-            <div className={s.headerMarker}>
+    let [active, setActive] = useState(false);
 
+    const onClickHeaderHandler = () => {
+        setActive(!active);
+    }
+
+    return (
+        <div>
+            <div className={active ? s.headerActive : s.header}>
+                <div className={s.link}>
+                    <Link to={PATH.PRE_JUNIOR}>pre-junior</Link>
+                </div>
+                <div className={s.link}>
+                    <Link to={PATH.JUNIOR}>junior</Link>
+                </div>
+                <div className={s.link}>
+                    <Link to={PATH.JUNIOR_PLUS}>junior+</Link>
+                </div>
+                <div className={s.headerMarker} onClick={onClickHeaderHandler}></div>
             </div>
         </div>
+
     )
 }
 
