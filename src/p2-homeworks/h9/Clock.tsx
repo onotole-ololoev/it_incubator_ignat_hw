e3c17b7e767b1caf7c2d3cbad3f20cce681bbdd4
Anatoli Nikolaevich <tolya.tar@gmail.com>
2022-05-30 11:40:42 +0300

    clocks in propgress

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,31 +1,45 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
         // stop
+
+        clearTimeout(timerId);
     }
+    // useEffect(() => {
+    //     clearTimeout(timerId);
+    // }, []);
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
             // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
+
+
     const onMouseEnter = () => {
         // show
+
+        // my code
+        setShow(!show)
     }
     const onMouseLeave = () => {
         // close
+        setShow(!show)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    // const stringTime = 'Time' // fix with date
+    // const stringDate = 'Date' // fix with date
+    const stringTime = new Date().toLocaleTimeString();
+    const stringDate = new Date().toLocaleDateString();
 
     return (
         <div>
